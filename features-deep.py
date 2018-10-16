c941be30781b8a6c26816797b80c41be7d777475
davi bernardo <davibernardo@MacBook-Air-de-davi.local>
2018-10-16 20:31:39 -0300

    Update features-deep.py
    Alteração na escrita de arquivos e localização do diretório de imagens

--- features-deep.py
+++ features-deep.py
@@ -10,51 +10,71 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
+import os
 
-#treinamento do modelo
+n_caracteristicas_coletadas = 512 + 1
+#n_caracteristicas_coletadas = 3585 + 1
+total_de_imagens = 1000 + 1
+intervalo_de_escrita = 2
+texto = ""
+caminho_img = os.path.abspath("imagens")
+
+try:
+    os.remove("saidaFeatures.csv")
+    print("\n << O arquivo csv antigo foi removido >>")
+except FileNotFoundError:
+    print("\n> A cada {} imagens o arquivo csv será atualizado.".format(intervalo_de_escrita))
+
+
+print("\n> Iniciando o treinamento do modelo...")
 model = VGG16(weights='imagenet', include_top=False)
 
-#armazenamento temporário de características
-texto = ""
-textoin = ""
-
-for i in range(1,513):
+print("\n> Criando o cabeçalho do aquivo csv com {} colunas...".format(n_caracteristicas_coletadas))
+for i in range(1,n_caracteristicas_coletadas):
     texto +=  str(i) + ","
 texto += "class\n"
 
-#passa pelo repositório de imagens
-for a in range(1,1001):
+if os.path.exists(caminho_img):
+    print("\n> Diretório de imagens ok.")
     
-    #as imagens estão soltas no diretório corrente 
-    #para evitar as diferenças de sistema operacional 
-    img_path = str(a-1) + '.jpg'
-    print("processando: " + img_path)
-    
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    
-    features = model.predict(x)
+    print("\n> Iniciando a extração de caracteristicas de {} imagens...".format(total_de_imagens))
+    for a in range(1,total_de_imagens):
         
-    #extrai características (apenas parte delas)
-    for i in range(len(features[0][0][0])):
-        texto += (str(features[0][0][0][i]))
-        texto += (",")
-    
-    classe = int(a/100)
-    print("classe: " + str(classe))
-    texto += (str(classe) + "\n")
-    
-    if a % 10 == 0:
-        arqin = open("saidaFeatures.csv", "r")
-        textoin = arqin.read()
-        textoin += texto
-        texto = "" 
+        #as imagens estão soltas no diretório corrente 
+        #para evitar as diferenças de sistema operacional
+        if os.name == "posix":
+            img_path = "{}/{}.jpg".format(caminho_img, str(a-1))
+        else:
+            img_path = "{}\{}.jpg".format(caminho_img, str(a-1))
+       
+        print("processando: {}".format(os.path.basename(img_path)))
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+            
+        features = model.predict(x)
+        
+        #extrai características (apenas parte delas)
+        for i in range(len(features[0][0][0])):
+            for j in range(len(features[0][0])):
+                texto += (str(features[0][0][j][i]))
+                texto += (",")
+            
+        classe = int(a/100)
+        print("classe: " + str(classe))
+        texto += (str(classe) + "\n")
+            
+        if a % intervalo_de_escrita == 0:
+            arq = open("saidaFeatures.csv", "a")
+            arq.write(str(texto))
+            arq.close()
+            texto = ""
+            
+else:
+    print("\n << Você precisa criar um diretório com o nome 'imagens'!! >>") 
+    print("Cuidado, o arquivo que o professor disponibilizou tem uma extensão '.org' que poderá causar problemas. >>")
 
-        arqout = open("saidaFeatures.csv", "w")
-        arqout.write(str(textoin))
-        arqout.close()
 
 
 '''
